d26331d0045f136a73a0791009cb3360a35cb739
Paul Hiemstra <paul.hiemstra@knmi.nl>
2011-11-10 13:23:37 +0000

    Wrote the readListGradsFiles2dataframe function

--- R/readListGradsFiles2dataframe.R
+++ R/readListGradsFiles2dataframe.R
@@ -1,18 +1,20 @@
 #' Reads a list of grads files into a data.frame
 #' 
-#' Reads a list of grads files into a data.frame
+#' Reads a list of grads files into an array and then
+#' uses melt to transform the array to a data.frame
 #' 
-#' @note This function is not yet implemented
+#' @param filelist character vector providing the locations of the grads files
+#'        are to be read.
+#' @param ts numeric vector expressing which timesteps should be read, e.g. ts = 1:10.
+#' @param lev numeric vector expressing which levels should be read, e.g. lev = c(200, 500).
+#' @param var character vector expressing which variable(s) should be read, e.g. var = "psi".
+#' @param verbose logical, if TRUE, the function provides feedback on what it is doing.
+#' @return a data.frame with the requested data.
 #' @author Paul Hiemstra, \email{p.h.hiemstra@@gmail.com}
 #' @export
-#' @examples
-#' 
-#' print("Hello World")
-#' 
-#' 
 readListGradsFiles2dataframe <-
-#function(filelist, ts, lev, var, verbose) {
-function() {
-  stop("Unimplemented yet!")
+function(filelist, ts, lev, var, verbose) {
+  dum = readListGradsFiles2array(filelist = filelist, ts = ts, lev = lev, var = var, verbose = verbose)
+  return(melt(dum))
 }
 
